46b547c34baeb0b380b45652088c9364a5b3987b
bailiek-netizen <bailiek@wsc.school.nz>
2026-04-01 18:23:33 +1300

    finishing touches

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function dark_mode(){
 document.body.style.backgroundColor = "black";
-
 };
 function light_mode(){
 document.body.style.backgroundColor = "white";
@@ -12,31 +11,5 @@ document.body.style.backgroundColor = "rgb(161, 211, 211)";
  };
 function Orange(){
 document.body.style.backgroundColor = "coral";
-
  };
-  function RandomGenerate() {
-   const pages = [
-      "CHERUB.html",
-      "divergent.html",
-      "HungerGames.html",
-      "PercyJackson.html",
-      "shatterMe.html",
-      "TheLoop.html"];
-const randomIndex = Math.floor(Math.random() * pages.length);
-  const randomPage = pages[randomIndex];
-  window.location.href = randomPage;
-} ;
- 
- //function RandomGenerate(pages){
-   // const randomIndex = Math.floor(Math.random() * pages.length);
-// const randomPage = pages[CHERUB.html];
-  //Math.random="random number"
-  // window.location.href = randomPage;
-//let page1="CHERUB.html";
-//let page2="divergent.html";
-//let page3="HungerGames.html";
-//let page4="PercyJackson.html";
-//let page5="shatterMe.html";
-//let page6="TheLoop.html";
-  // };
-   
+
