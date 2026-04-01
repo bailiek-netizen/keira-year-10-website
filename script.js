3ba5e1a3bb2b9b54f51647b5dbd8bd4b11d2a24d
bailiek-netizen <bailiek@wsc.school.nz>
2026-04-01 14:06:23 +1300

    made my javasript buttons work

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function dark_mode(){
 document.body.style.backgroundColor = "black";
 
 };
+function light_mode(){
+document.body.style.backgroundColor = "white";
+
+};
+function Reset_background(){
+document.body.style.backgroundColor = "rgb(161, 211, 211)";
+
+};
+function Orange(){
+document.body.style.backgroundColor = "coral";
+
+};
