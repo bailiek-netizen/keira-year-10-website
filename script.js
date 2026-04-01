ac660039a65eac685ed31e80f53baf360713f39a
bailiek-netizen <bailiek@wsc.school.nz>
2026-04-01 15:09:07 +1300

    added javascript
    added javascript

--- script.js
+++ script.js
@@ -9,8 +9,24 @@ document.body.style.backgroundColor = "white";
 function Reset_background(){
 document.body.style.backgroundColor = "rgb(161, 211, 211)";
 
-};
+ };
 function Orange(){
 document.body.style.backgroundColor = "coral";
 
-};
+ };
+   
+ function RandomGenerate(pages){
+  //const pages = ["CHERUB.html","divergent.html","HungerGames.html","PercyJackson.html","shatterMe.html","TheLoop.html"];
+    const randomIndex = Math.floor(Math.random() * pages.length);
+   
+ const randomPage = pages[CHERUB.html];
+  Math.random="random number"
+   window.location.href = randomPage;
+   let page1="CHERUB.html";
+let page2="divergent.html";
+let page3="HungerGames.html";
+let page4="PercyJackson.html";
+let page5="shatterMe.html";
+let page6="TheLoop.html";
+   };
+   
